05d4092c77b8e983dcb453d64a19ad49df9561ac
Konrad Nölle <konrad@familie-noelle.de>
2022-12-09 13:53:00 +0100

    Add day-09 part 2

--- 2022/day-09/src/main.rs
+++ 2022/day-09/src/main.rs
@@ -38,18 +38,20 @@ fn apply_head_steps(steps: &[Position]) -> Vec<Position> {
 fn chase_head(positions: &[Position]) -> Vec<Position> {
     once((0, 0))
         .chain(positions.iter().scan((0, 0), |tail, head| {
-            println!("{head:?} {tail:?}");
             match (head.0 - tail.0, head.1 - tail.1) {
-                (x @ 2 | x @ -2, _) => {
-                    tail.0 += x / 2;
+                (x, y) if x.abs() < 2 && y.abs() < 2 => {}
+                (x, y) if x.abs() == y.abs() => {
+                    tail.0 += x - x.signum();
+                    tail.1 += y - y.signum();
+                }
+                (x, y) if x.abs() > y.abs() => {
+                    tail.0 += x - x.signum();
                     tail.1 = head.1;
                 }
-                (_, y @ 2 | y @ -2) => {
+                (_x, y) => {
                     tail.0 = head.0;
-                    tail.1 += y / 2;
+                    tail.1 += y - y.signum();
                 }
-                (x, y) if x.abs() > 2 || y.abs() > 2 => panic!("Invalid offset: {x} {y}"),
-                _ => {}
             }
             Some(*tail)
         }))
@@ -67,6 +69,15 @@ fn main() {
     let positions = apply_head_steps(&steps);
     let tail_positions = chase_head(&positions);
     println!("Unique positions: {}", count_unique(&tail_positions));
+
+    let mut positions = apply_head_steps(&steps);
+    for _ in 0..9 {
+        positions = chase_head(&positions);
+    }
+    println!(
+        "Unique positions with 10 knots: {}",
+        count_unique(&positions)
+    );
 }
 
 #[cfg(test)]
@@ -82,5 +93,31 @@ mod test {
         let tail_positions = chase_head(&positions);
         println!("Unique positions: {}", count_unique(&tail_positions));
         assert_eq!(count_unique(&tail_positions), 13);
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = read_to_string("example").unwrap();
+        let steps = parse_steps(&input);
+
+        let mut positions = apply_head_steps(&steps);
+        for _ in 0..9 {
+            positions = chase_head(&positions);
+        }
+        println!("Unique positions: {}", count_unique(&positions));
+        assert_eq!(count_unique(&positions), 1);
+    }
+
+    #[test]
+    fn large_example_part_2() {
+        let input = read_to_string("example2").unwrap();
+        let steps = parse_steps(&input);
+
+        let mut positions = apply_head_steps(&steps);
+        for _ in 0..9 {
+            positions = chase_head(&positions);
+        }
+        println!("Unique positions: {}", count_unique(&positions));
+        assert_eq!(count_unique(&positions), 36);
     }
 }
