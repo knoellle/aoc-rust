7d3787948dc996556a416382d4dfb82e5d5e8478
knoellle <konrad@familie-noelle.de>
2022-12-17 01:24:47 +0100

    Add day-15 part 2

--- 2022/day-15/src/main.rs
+++ 2022/day-15/src/main.rs
@@ -1,6 +1,7 @@
-use std::{collections::HashSet, fs::read_to_string, num::ParseIntError};
+use std::{collections::HashSet, fs::read_to_string};
 
 use eyre::{Context, Result};
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 use regex::Regex;
 
 type Position = (i64, i64);
@@ -34,6 +35,10 @@ impl Sensor {
 
     fn in_range(&self, position: Position) -> bool {
         manhattan(self.position, position) <= self.clearing_distance
+    }
+
+    fn range_on_row(&self, y: i64) -> i64 {
+        self.clearing_distance - (self.position.1 - y).abs()
     }
 }
 
@@ -80,6 +85,100 @@ fn blockers_in_row(sensors: &[Sensor], y: i64) -> usize {
     count - beacons.len()
 }
 
+// too slow
+fn find_distress_signal_brute_force(
+    sensors: &[Sensor],
+    min: Position,
+    max: Position,
+) -> Option<Position> {
+    (min.0..=max.0).into_par_iter().find_map_any(|x| {
+        (min.1..=max.1)
+            .into_par_iter()
+            .find_any(|y| !sensors.iter().any(|sensor| sensor.in_range((x, *y))))
+            .map(|y| (x, y))
+    })
+}
+
+enum Transition {
+    Enter,
+    Exit,
+}
+
+// doesn't work
+fn find_distress_signal_events(
+    sensors: &[Sensor],
+    min: Position,
+    max: Position,
+) -> Option<Position> {
+    for y in min.1..=max.1 {
+        let mut events: Vec<_> = sensors
+            .iter()
+            .flat_map(|sensor| {
+                [
+                    (
+                        sensor.position.0 - sensor.range_on_row(y),
+                        Transition::Enter,
+                    ),
+                    (sensor.position.0 + sensor.range_on_row(y), Transition::Exit),
+                ]
+            })
+            .collect();
+        events.sort_by_key(|event| event.0);
+        events
+            .iter()
+            .scan(0, |state, event| {
+                *state += match event.1 {
+                    Transition::Enter => 1,
+                    Transition::Exit => -1,
+                };
+                (*state == 0).then_some(event)
+            })
+            .skip_while(|event| event.0 < min.0)
+            .take_while(|event| event.0 <= max.0)
+            .next()
+            .map(|x| (y, x));
+    }
+
+    None
+}
+
+// wörks
+fn find_distress_signal_borders(
+    sensors: &[Sensor],
+    min: Position,
+    max: Position,
+) -> Option<Position> {
+    let mut positions = sensors
+        .iter()
+        // .take(1)
+        .flat_map(|sensor| {
+            // println!("range: {}", sensor.clearing_distance + 1);
+            [(0, 1, 1, 1), (1, 0, -1, 1), (0, -1, -1, -1), (-1, 0, 1, -1)]
+                .iter()
+                .flat_map(|border| {
+                    (0..sensor.clearing_distance).map(|i| {
+                        (
+                            sensor.position.0
+                                + border.0 * (sensor.clearing_distance + 1)
+                                + i * border.2,
+                            sensor.position.1
+                                + border.1 * (sensor.clearing_distance + 1)
+                                + i * border.3,
+                        )
+                    })
+                })
+        })
+        // .inspect(|position| println!("{position:?}"))
+        .filter(|position| {
+            (min.0..=max.0).contains(&position.0) && (min.1..=max.1).contains(&position.1)
+        });
+    positions.find(|position| !sensors.iter().any(|sensor2| sensor2.in_range(*position)))
+}
+
+fn tuning_frequency(position: Position) -> i64 {
+    position.0 * 4_000_000 + position.1
+}
+
 fn main() {
     let input = read_to_string("input").unwrap();
     let sensors: Vec<Sensor> = input
@@ -88,6 +187,9 @@ fn main() {
         .collect();
 
     println!("Part 1: {}", blockers_in_row(&sensors, 2000000));
+    let position = find_distress_signal_borders(&sensors, (0, 0), (4_000_000, 4_000_000));
+    println!("Part 2: {:?}", position);
+    println!("Tuning frequency: {}", tuning_frequency(position.unwrap()));
 }
 
 #[cfg(test)]
@@ -103,5 +205,8 @@ mod test {
             .inspect(|sensor| println!("{sensor:?}"))
             .collect();
         assert_eq!(blockers_in_row(&sensors, 10), 26);
+        let position = find_distress_signal_borders(&sensors, (0, 0), (20, 20));
+        assert_eq!(position, Some((14, 11)));
+        assert_eq!(tuning_frequency(position.unwrap()), 56000011);
     }
 }
